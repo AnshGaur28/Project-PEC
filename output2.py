213a16b5c700a32e544e4910719fd88c0af3662e
AnshGaur28 <96479076+AnshGaur28@users.noreply.github.com>
2022-01-10 17:38:44 +0530

    Add files via upload

---
+++ output2.py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\Dell\PycharmProjects\Introductiontocomputing\question2.py =
+what is your gross income ? 50000
+how many dependents do you have ? 3
+31000
+6200.0
---
+++ output4.py
@@ -0,0 +1,11 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\Dell\PycharmProjects\Introductiontocomputing\question4.py =
+marks of student1: 10
+marks of student2: 9
+marks of student3: 8
+marks of student4: 12
+marks of student5: 15
+[10, 9, 8, 12, 15]
+list after sorting [8, 9, 10, 12, 15]
